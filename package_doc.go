d0730e4da130099cf73bc16e32cdcfb59e2cd727
Sergey Grebenshchikov <sgreben@gmail.com>
2018-05-19 17:16:54 +0200

    update package doc

--- package_doc.go
+++ package_doc.go
@@ -1,2 +1,3 @@
-// Package flagvar provides flag argument types for the standard `flag` package
+// Package flagvar provides flag argument types for the standard `flag` package.
+// All types implement `flag.Value` can be passed to `flag.Var()`.
 package flagvar
